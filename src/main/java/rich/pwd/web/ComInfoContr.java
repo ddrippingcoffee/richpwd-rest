9da48ded6a872f996f7596745bf22d742380a169
Larry <ddrippingcoffee@gmail.com>
2022-10-14 09:53:13 +0800

    新增 ComInfo StEntry controller

---
+++ src/main/java/rich/pwd/web/ComInfoContr.java
@@ -0,0 +1,49 @@
+package rich.pwd.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rich.pwd.bean.po.ComInfo;
+import rich.pwd.serv.intf.ComInfoServ;
+
+@RestController
+@RequestMapping("cominfo")
+public class ComInfoContr {
+
+  private final ComInfoServ comInfoServ;
+
+  @Autowired
+  public ComInfoContr(ComInfoServ comInfoServ) {
+    this.comInfoServ = comInfoServ;
+  }
+
+  @PostMapping("/")
+  @CrossOrigin(origins = "*", allowedHeaders = "*")
+  public ResponseEntity<?> save(@RequestBody ComInfo comInfo) {
+    // 自增主鍵會自動填入
+    comInfoServ.save(comInfo);
+    return new ResponseEntity<>(null, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/")
+  @CrossOrigin(origins = "*", allowedHeaders = "*")
+  public ResponseEntity<?> getAll() {
+    return new ResponseEntity<>(comInfoServ.findAll(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{symb}")
+  public ResponseEntity<?> getBySymb(@PathVariable String symb) {
+    return new ResponseEntity<>(comInfoServ.findOneBySymb(symb), HttpStatus.OK);
+  }
+
+  @GetMapping("/s/nm")
+  public ResponseEntity<?> getByComNm(@RequestParam String nm) {
+    return new ResponseEntity<>(comInfoServ.findOneByComNm(nm), HttpStatus.OK);
+  }
+
+  @GetMapping("/s/indus")
+  public ResponseEntity<?> getByComIndus(@RequestParam String indus) {
+    return new ResponseEntity<>(comInfoServ.findAllByComIndus(indus), HttpStatus.OK);
+  }
+}
---
+++ src/main/java/rich/pwd/web/StEntryContr.java
@@ -0,0 +1,43 @@
+package rich.pwd.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rich.pwd.bean.po.StEntry;
+import rich.pwd.serv.intf.StEntryServ;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("entry")
+public class StEntryContr {
+
+  private final StEntryServ stEntryServ;
+
+  @Autowired
+  public StEntryContr(StEntryServ stEntryServ) {
+    this.stEntryServ = stEntryServ;
+  }
+
+  @PostMapping("/")
+  public ResponseEntity<?> save(@RequestBody StEntry entry) {
+    // 自增主鍵會自動填入
+    entry.setC8tDtm(LocalDateTime.now());
+    stEntryServ.saveAndFlush(entry);
+    return new ResponseEntity<>(null, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/act")
+  public ResponseEntity<?> getAllActiveEntry() {
+    List<StEntry> stEntryList = stEntryServ.getAllActiveEntry();
+    return new ResponseEntity<>(stEntryList, HttpStatus.OK);
+  }
+
+  @GetMapping("/old")
+  public ResponseEntity<?> getAllOldEntry() {
+    List<StEntry> stEntryList = stEntryServ.getAllOldEntry();
+    return new ResponseEntity<>(stEntryList, HttpStatus.OK);
+  }
+}
