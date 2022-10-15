1386687d60ba416d03bb2ca91cbac7fdc59c24b5
Larry <ddrippingcoffee@gmail.com>
2022-10-15 15:32:32 +0800

    加入 @EnableWebSecurity 註解

--- src/main/java/rich/pwd/config/WebSecurityConfig.java
+++ src/main/java/rich/pwd/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,7 +20,7 @@ import rich.pwd.config.jwt.CustomAccessDeniedHandler;
 import rich.pwd.config.jwt.UserDetailsServiceImpl;
 
 @Configuration
-// @EnableWebSecurity
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(
         // securedEnabled = true,
         // jsr250Enabled = true,
