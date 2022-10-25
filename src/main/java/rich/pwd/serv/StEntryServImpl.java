166fa67af707899536d3fae3aa24f0b3d4e4e994
Larry <ddrippingcoffee@gmail.com>
2022-10-26 04:17:16 +0800

    新增 StComEntryVo 回傳物件

--- src/main/java/rich/pwd/serv/StEntryServImpl.java
+++ src/main/java/rich/pwd/serv/StEntryServImpl.java
@@ -2,23 +2,39 @@ package rich.pwd.serv;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import rich.pwd.bean.po.ComInfo;
 import rich.pwd.bean.po.StEntry;
+import rich.pwd.bean.vo.StComEntryVo;
 import rich.pwd.repo.StEntryDao;
+import rich.pwd.serv.intf.ComInfoServ;
 import rich.pwd.serv.intf.StEntryServ;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StEntryServImpl extends BaseServImpl<StEntry, Long, StEntryDao> implements StEntryServ {
 
-  public StEntryServImpl(StEntryDao repository) {
+  private final ComInfoServ comInfoServ;
+
+  public StEntryServImpl(StEntryDao repository, ComInfoServ comInfoServ) {
     super(repository);
+    this.comInfoServ = comInfoServ;
   }
 
   @Override
-  public List<StEntry> getAllActiveEntry() {
-    return super.getRepository().findAllByDelDtmIsNullOrderByC8tDtmDesc();
+  public List<StComEntryVo> getAllActiveComEntry() {
+    List<StEntry> entryList = super.getRepository()
+            .findAllByDelDtmIsNullOrderByC8tDtmDesc();
+    return entryList.stream().map(entry -> {
+      ComInfo comInfo = comInfoServ.findOneBySymb(entry.getSymb());
+      return new StComEntryVo(entry,
+              entry.getStDtlList(),
+              comInfo.getComNm(),
+              comInfo.getComType(),
+              comInfo.getComIndus());
+    }).collect(Collectors.toList());
   }
 
   @Override
